da5e0593e7e91f4d39d11cf2646a1363cea410d7
Troj <trojandersen@gmail.com>
2024-05-16 15:11:27 +0200

    some project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
